035e8430e669b8f87a9808796172da25a8256311
maxmorev <maxmorev@gmail.com>
2019-03-26 01:52:48 +0300

    refactoring

--- src/main/java/ru/maxmorev/cloud/sap/ApplicationREST.java
+++ src/main/java/ru/maxmorev/cloud/sap/ApplicationREST.java
@@ -2,17 +2,18 @@ package ru.maxmorev.cloud.sap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class ApplicationREST extends SpringBootServletInitializer {
 
 	// Make sure that everything for .war deployment is there
+	/*
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(ApplicationREST.class);
 	}
+	*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApplicationREST.class, args);
