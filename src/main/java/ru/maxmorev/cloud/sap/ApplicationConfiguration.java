4737f4f6c67cb139c015197597103471035566ec
maxmorev <maxmorev@gmail.com>
2019-04-29 00:07:46 +0300

    Update config for tomcat 9

--- src/main/java/ru/maxmorev/cloud/sap/ApplicationConfiguration.java
+++ src/main/java/ru/maxmorev/cloud/sap/ApplicationConfiguration.java
@@ -2,10 +2,13 @@ package ru.maxmorev.cloud.sap;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableMBeanExport;
+import org.springframework.jmx.support.RegistrationPolicy;
 import ru.maxmorev.cloud.sap.service.OpenweatherTemperatureProvider;
 import ru.maxmorev.cloud.sap.service.TemperatureProvider;
 
 @Configuration
+@EnableMBeanExport(registration= RegistrationPolicy.IGNORE_EXISTING)
 public class ApplicationConfiguration {
 
     @Bean
